6df6da3bd37d3f2fd89f1478c01ad785dd24c11c
Anish Chakraborty <anishchakrabort699@gmail.com>
2026-01-07 01:27:27 +0530

    Refactor layout and fix visual hierarchy

--- js/main.js
+++ js/main.js
@@ -2,6 +2,5 @@ function showSection(id) {
   document.querySelectorAll('.content-box').forEach(box => {
     box.classList.remove('active');
   });
-
   document.getElementById(id).classList.add('active');
 }
