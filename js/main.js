d83e0245c62d88b3c313db3b0143a730c3a4c54a
Anishchakraborty7 <anishchakraborty699@gmail.com>
2026-01-07 11:19:58 +0530

    Update main.js

--- js/main.js
+++ js/main.js
@@ -4,4 +4,19 @@ function showSection(id) {
   });
   document.getElementById(id).classList.add('active');
 }
+function openHireModal() {
+  document.getElementById("hireModal").classList.add("active");
+}
+
+function closeHireModal() {
+  document.getElementById("hireModal").classList.remove("active");
+}
+
+/* Close popup when clicking outside */
+document.addEventListener("click", function (e) {
+  const modal = document.getElementById("hireModal");
+  if (e.target === modal) {
+    closeHireModal();
+  }
+});
 
