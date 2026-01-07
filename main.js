35d2521058dc8ea1f6f719053239d96f9f582f66
Anishchakraborty7 <anishchakraborty699@gmail.com>
2026-01-07 11:06:15 +0530

    Update main.js

--- main.js
+++ main.js
@@ -5,3 +5,19 @@ function toggleSection(el) {
 document.getElementById("toggleTheme")?.addEventListener("click", () => {
   document.body.classList.toggle("dark");
 });
+function openHireModal() {
+  document.getElementById("hireModal").classList.add("active");
+}
+
+function closeHireModal() {
+  document.getElementById("hireModal").classList.remove("active");
+}
+
+/* Close popup when clicking outside */
+document.addEventListener("click", function (e) {
+  const modal = document.getElementById("hireModal");
+  if (e.target === modal) {
+    closeHireModal();
+  }
+});
+
